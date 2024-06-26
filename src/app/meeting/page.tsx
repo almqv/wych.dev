d89a889cc72622dc429522d08e63144335670937
Elias Almqvist <elalmqvist@gmail.com>
2024-06-26 18:41:01 +0200

    metadata for scheduling etc

--- src/app/meeting/page.tsx
+++ src/app/meeting/page.tsx
@@ -1,12 +1,25 @@
-"use client";
-
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const SCHEDULING_URL = "https://cal.com/elias-almqvist-d33wxo";
 
+export const metadata = {
+  title: "Schedule a meeting with Elias",
+  description: "Schedule a meeting with Elias Almqvist",
+};
+
 // Redirect to the external URL
 const Page = () => {
   redirect(SCHEDULING_URL);
+
+  return (
+    <div className="h-full flex flex-col justify-center items-center space-y-2">
+      <span className="animate-pulse text-2xl font-mono">
+        Redirecting to the scheduling page...
+      </span>
+      <Link href={SCHEDULING_URL}>Click here if you are not redirected</Link>
+    </div>
+  );
 };
 
 export default Page;
